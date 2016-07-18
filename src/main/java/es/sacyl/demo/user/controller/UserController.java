1f81be78cb00850d42bfe4a0ef01941b89f780b3
Isidro Merayo Castellano <imerayo@saludcastillayleon.es>
2016-07-18 14:50:52 +0200

    Mock static methods

--- src/main/java/es/sacyl/demo/user/controller/UserController.java
+++ src/main/java/es/sacyl/demo/user/controller/UserController.java
@@ -3,6 +3,9 @@
  */
 package es.sacyl.demo.user.controller;
 
+import java.util.UUID;
+
+import es.sacyl.demo.user.domain.User;
 import es.sacyl.demo.user.service.interfaces.UserService;
 
 /**
@@ -21,11 +24,24 @@ public class UserController {
 		this.userService = userService;
 	}
 
+	public UserController() {
+		// TODO Auto-generated constructor stub
+	}
+
 	/**
 	 * 
 	 * @return Long
 	 */
 	public Long getUserCount() {
 		return userService.getUserCount();
+	}
+
+	/**
+	 * 
+	 * @param user
+	 * @return String
+	 */
+	public String createUserId(User user) {
+		return String.format("%s_%s", user.getSurname(), UUID.randomUUID().toString());
 	}
 }
--- src/test/java/es/sacyl/unit/demo/user/controller/UserControllerTest.java
+++ src/test/java/es/sacyl/unit/demo/user/controller/UserControllerTest.java
@@ -5,13 +5,19 @@ package es.sacyl.unit.demo.user.controller;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.UUID;
+
+import org.easymock.EasyMock;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.powermock.api.easymock.PowerMock;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 import es.sacyl.demo.user.controller.UserController;
+import es.sacyl.demo.user.domain.User;
 import es.sacyl.demo.user.service.DefaultUserService;
 
 /**
@@ -26,10 +32,27 @@ public class UserControllerTest {
 	private UserController userController;
 
 	@Test
-	public void testGetUserCount() {
+	public void getUserCount() {
 		mockUserService = PowerMockito.mock(DefaultUserService.class);
 		PowerMockito.when(mockUserService.getUserCount()).thenReturn(100L);
 		userController = new UserController(mockUserService);
 		assertEquals(100L, userController.getUserCount().longValue());
+	}
+
+	@Test
+	public void mockStatic() throws Exception {
+		PowerMock.mockStaticPartial(UUID.class, "randomUUID");
+		EasyMock.expect(UUID.randomUUID()).andReturn(UUID.fromString("067e6162-3b6f-4ae2-a171-2470b63dff00"));
+		PowerMock.replayAll();
+		UserController userController = new UserController();
+		Assert.assertTrue(userController.createUserId(getNewUser()).contains("067e6162-3b6f-4ae2-a171-2470b63dff00"));
+		PowerMock.verifyAll();
+	}
+
+	private User getNewUser() {
+		User user = new User();
+		user.setFirstName("Code");
+		user.setSurname("Geeks");
+		return user;
 	}
 }
